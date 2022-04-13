e615beeb585c678558ee5617d4b91f5595c7aa47
최정필 <wjdvlf1407@gmail.com>
2022-04-13 14:25:36 +0900

    1.2 04/13 4교시종료

--- src/com/javaex/ex01/Ex01.java
+++ src/com/javaex/ex01/Ex01.java
@@ -6,74 +6,56 @@
 package com.javaex.ex01;
 
 public class Ex01 {
-	
-	public static void main (String[] args) {
-		
-		
-		//변수선언 + 초기화
+
+	public static void main(String[] args) {
+
+		// 변수선언 + 초기화
 		int myAge = 25;
-		
+
 		/*
-		int myAge;
-		myAge = 25;
-		*/
-		//메모리의 값을 출력하는 메소드
+		int myAge; myAge = 25;
+		 */
+		// 메모리의 값을 출력하는 메소드
 		System.out.println(myAge);
-		
+
 		myAge = 26;
 		System.out.println(myAge);
-		
+
 		byte no;
 		no = -128;
-		System.out.println (no);
-		
+		System.out.println(no);
+
 //		int 범위까지는 L을 붙이지 않아도 된다.
 //		int 범위를 벗어나느 경우 L을 붙인다.
 		long no2;
 		no2 = 122337667;
-		System.out.println (no2);
-		
+		System.out.println(no2);
+
 		///////////////////////////////////////////
-		//변수선언, 초기화
+		// 변수선언, 초기화
 		///////////////////////////////////////////
-		
-		
-		//변수여러개일 때 변수선언, 초기화
-		
+
+		// 변수여러개일 때 변수선언, 초기화
+
 		int var01;
 		int var02;
 		int var03;
-		//int var01, var02, var03;
-		
+		// int var01, var02, var03;
+
 		var01 = 10;
 		var02 = 20;
 		var03 = 30;
-		
-		//변수여러개일 때 변수+초기화
+
+		// 변수여러개일 때 변수+초기화
 		int var04 = 10;
 		int var05 = 20;
 		int var06 = 30;
-		//int var04 = 10, var05 = 20, var06 = 30;
+		// int var04 = 10, var05 = 20, var06 = 30;
+		System.out.println(var04);
+		System.out.println(var05);
+		System.out.println(var06);
+		
+		
+		
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/com/javaex/ex01/Ex02.java
+++ src/com/javaex/ex01/Ex02.java
@@ -4,9 +4,12 @@ public class Ex02 {
 
 	public static void main(String[]args) {
 	
-		double var01;
-		var01 = 3.14;
+		double var01 = 3.14;
+		float var02= 3.14f;
+		
 		System.out.println(var01);
+		System.out.println(var02);
+		
 	}
 	
 }
