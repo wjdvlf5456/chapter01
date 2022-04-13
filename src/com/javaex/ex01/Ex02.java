56c39dcf1c010e1183130987f0e09b81380b86be
최정필 <wjdvlf1407@gmail.com>
2022-04-13 14:56:25 +0900

    1.31 논리형 기초

--- src/com/javaex/ex01/Ex02.java
+++ src/com/javaex/ex01/Ex02.java
@@ -6,10 +6,14 @@ public class Ex02 {
 	
 		double var01 = 3.14;
 		float var02= 3.14f;
-		
+	
 		System.out.println(var01);
 		System.out.println(var02);
 		
+		double var03 = 0.123456789123456789;
+		float var04 =  0.123456789123456789f;
+		
+		System.out.println(var03);
+		System.out.println(var04);
 	}
-	
 }
---
+++ src/com/javaex/ex01/Ex03.java
@@ -0,0 +1,14 @@
+package com.javaex.ex01;
+
+public class Ex03 {
+
+    public static void main(String[]args){
+	
+	boolean b01 = true;
+	boolean b02 = false;
+	
+	System.out.println(b01);
+	System.out.println(b02);
+	
+    }
+}
