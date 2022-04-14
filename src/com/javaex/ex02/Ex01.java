6261eaf0700d28837a8e1a08f543a5f16796a355
최정필 <wjdvlf1407@gmail.com>
2022-04-14 16:41:00 +0900

    1.0 자바 콘솔

---
+++ src/com/javaex/ex02/Ex01.java
@@ -0,0 +1,23 @@
+package com.javaex.ex02;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		int i =2345;
+		String str = "굿모닝";
+		
+		System.out.println("안녕하세요");
+		System.out.print(str);
+		
+		System.out.print("안녕");
+		System.out.print("하세요");
+		
+		
+		System.out.println(str + str + "안녕하세요");
+		System.out.println(str + i);
+		String var01 = str+i;
+		System.out.println(var01);
+	}
+
+}
