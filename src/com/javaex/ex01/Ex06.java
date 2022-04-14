1e5c37d98b5b29a0fd04f749bbbdfbf0a458ab29
최정필 <wjdvlf1407@gmail.com>
2022-04-14 11:13:38 +0900

    1.6 자동형변환

---
+++ src/com/javaex/ex01/Ex06.java
@@ -0,0 +1,22 @@
+package com.javaex.ex01;
+
+	public class Ex06 {
+		
+		public static void main (String[] args) {
+			
+			//자동형변환 1
+			System.out.println(2+3.5);
+			
+			double var01 = 2+3.5;   //2-->2.0 으로 자동형변환됨
+			System.out.println(var01);
+			
+			//자동형변환 2
+			long var02 = 12345l;
+			float var03 = 1.1f;
+			
+			System.out.println(var02+var03);
+			
+			float result = var02 + var03; //var02(12345l) --> 12345.0f
+			System.out.println(result);
+		}
+	}
