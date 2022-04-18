9e09502f933ce18074069efd77df0453f363e24a
최정필 <wjdvlf1407@gmail.com>
2022-04-18 10:46:53 +0900

    2.142 Ex01.java 반복문2

--- src/com/javaex/ex04/Ex01.java
+++ src/com/javaex/ex04/Ex01.java
@@ -5,13 +5,11 @@ public class Ex01 {
 	public static void main(String[] args) {
 		
 		
-		int i = 1;
+		int i = 0;
 		
-		int num = 0;
 		
-		while (i<=5) { //6<=5	-->F					--> 몇 개 나올지 정함
-			System.out.println("I like java" + num);
-			num++;	//num=num+1;
+		while (i<=4) { //6<=5	-->F					--> 몇 개 나올지 정함
+			System.out.println("I like java" + i);
 			i++;	//i=i+1;
 			
 			
