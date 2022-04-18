f82f0b5192fd01e589a475ae80cc73e573b673e0
최정필 <wjdvlf1407@gmail.com>
2022-04-18 16:12:37 +0900

    2.172 for반복문 연습문제2  6*6

---
+++ src/com/javaex/ex04/Ex07.java
@@ -0,0 +1,74 @@
+package com.javaex.ex04;
+
+public class Ex07 {
+	
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=6; i++) {
+			
+			for (int j = 1; j < i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println("*");
+		}
+			
+	}
+
+}
+
+		
+		/*
+		for (int i = 1; i <= 1; i++) {
+			
+			for (int j = 1; j <=1; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		for (int i = 1; i <= 1; i++) {
+			
+			for (int j = 1; j <=2; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		for (int i = 1; i <= 1; i++) {
+			
+			for (int j = 1; j <=3; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		for (int i = 1; i <= 1; i++) {
+			
+			for (int j = 1; j <=4; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		for (int i = 1; i <= 1; i++) {
+			
+			for (int j = 1; j <=5; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+		for (int i = 1; i <= 1; i++) {
+			
+			for (int j = 1; j <=6; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		*/
+//	}
+	
+	
+
+//}
