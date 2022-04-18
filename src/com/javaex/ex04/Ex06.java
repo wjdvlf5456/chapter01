e11938e8b22784198c840d7f535d1ce0a6005362
최정필 <wjdvlf1407@gmail.com>
2022-04-18 15:08:58 +0900

    2.171 for반복문 *출력 6x6

---
+++ src/com/javaex/ex04/Ex06.java
@@ -0,0 +1,18 @@
+package com.javaex.ex04;
+
+public class Ex06 {
+	
+	public static void main(String[] args) {
+		
+		
+		for (int i = 1; i <=6; i++) {
+			
+			for (int j = 1; j <=6; j++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
