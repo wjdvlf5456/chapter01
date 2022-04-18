6e14805586f3e8c51d028c03a3956db7759e1d89
최정필 <wjdvlf1407@gmail.com>
2022-04-18 16:46:57 +0900

    2.182 연습문제 while반복문으로 작성

--- src/com/javaex/ex04/Ex08.java
+++ src/com/javaex/ex04/Ex08.java
@@ -5,14 +5,18 @@ public class Ex08 {
 	public static void main(String[] args) {
 		
 		
-		for (int i=1; i > 0; i++) {
+		int i=1;
+		
+		while (true) {
+			
+			//조건이 되면 탈출
 			if (i%6==0 && i%14==0) {
-				
+				//if문이 true
 				System.out.println(i);
 				break;
 			}
+			i++;
 		}
-
 			
 	}
 
