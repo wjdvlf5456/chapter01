83e5e7481542d5f3ffee4e5b8a47788869bbcf84
최정필 <wjdvlf1407@gmail.com>
2022-04-18 10:45:44 +0900

    2.141 반복문1

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,22 @@
+package com.javaex.ex04;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		
+		int i = 1;
+		
+		int num = 0;
+		
+		while (i<=5) { //6<=5	-->F					--> 몇 개 나올지 정함
+			System.out.println("I like java" + num);
+			num++;	//num=num+1;
+			i++;	//i=i+1;
+			
+			
+		}
+		
+	}
+
+}
