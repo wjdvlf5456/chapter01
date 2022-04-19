9c4ac96b0189646c9d78f973249651968b957896
최정필 <wjdvlf1407@gmail.com>
2022-04-19 15:04:32 +0900

    Ex11 for문 + continue문 예제 풀어보기

--- src/com/javaex/ex04/Ex11.java
+++ src/com/javaex/ex04/Ex11.java
@@ -1,16 +1,15 @@
 package com.javaex.ex04;
 
-public class Ex10 {
-	
+public class Ex11 {
+
 	public static void main(String[] args) {
-		
-		for (int i=1; i<=10; i++) {
-			if (i%2==0 && i%3==0) {
+
+		for (int i = 1; i <= 20; i++) {
+			if (i % 2 == 0 || i % 3 == 0) {
 				continue;
 			}
 			System.out.println(i);
 		}
-		
 	}
 
 }
