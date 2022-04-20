a784c5e9188a3a877b53dce50c81710c6d9855dd
최정필 <wjdvlf1407@gmail.com>
2022-04-20 15:55:24 +0900

    Ex06,Ex07 배열확인

---
+++ src/com/javaex/ex05/Ex07.java
@@ -0,0 +1,15 @@
+package com.javaex.ex05;
+
+public class Ex07 {
+	
+	public static void main(String[] args) {
+		
+		
+		int[] leftArray = new int[] {10, 20, 30};
+		int[] rightArray = new int[] {10, 20, 30};
+		
+		
+		
+	}
+
+}
