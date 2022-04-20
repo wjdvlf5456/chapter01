9484536810a9310e35a59b147b0830120558ab01
최정필 <wjdvlf1407@gmail.com>
2022-04-20 14:09:43 +0900

    Ex04 double을 이용하여 배열익히기

--- src/com/javaex/ex05/Ex04.java
+++ src/com/javaex/ex05/Ex04.java
@@ -3,18 +3,44 @@ package com.javaex.ex05;
 public class Ex04 {
 
 	public static void main(String[] args) {
+		
+		
+		//	정수형
+		/*
+		int[] intArray = new int[3];
+		intArray[0] = 3;
+		intArray[1] = 6;
+		intArray[2] = 9;
+		
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+		*/
 
-		String[] str = new String[5];
+		/*
+		int[] intArray = { 3, 6, 9 };
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+		*/
 
-		str[0] = "안";
-		str[1] = "녕";
-		str[2] = "하";
-		str[3] = "세";
-		str[4] = "요";
+		/*
+		int[] intArray = { 3, 6, 9 };
+		for (int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+		*/
+		System.out.println("==============================");
+		
+		//	실수형
+		double[] dArray = new double[3];
 
-		for (int i = 0; i < str.length; i++) {
-			System.out.print(str[i]);
+		dArray[0] = 1.1;
+		dArray[1] = 7.234234143;
+		dArray[2] = 5.345;
 
+		for (int i = 0; i < dArray.length; i++) {
+			System.out.println(dArray[i]);
 		}
 
 	}
