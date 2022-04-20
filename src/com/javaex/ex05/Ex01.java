84dbe544b25df042970327c7f1dd7ba0a947776b
최정필 <wjdvlf1407@gmail.com>
2022-04-20 10:22:11 +0900

    2022-04-20 배열 로또번호 만들기 1

---
+++ src/com/javaex/ex05/Ex01.java
@@ -0,0 +1,19 @@
+package com.javaex.ex05;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		int num = (int)(Math.random()*45)+1;
+		
+		
+		System.out.print(num+"\t");
+		System.out.print(num+"\t");
+		System.out.print(num+"\t");
+		System.out.print(num+"\t");
+		System.out.print(num+"\t");
+		System.out.print(num+"\t");
+		
+	}
+
+}
