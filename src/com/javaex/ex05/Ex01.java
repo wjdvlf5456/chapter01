529aa1a5a3a81f52e2f7727854d1bf638092be58
최정필 <wjdvlf1407@gmail.com>
2022-04-20 10:23:38 +0900

    Ex01 로또번호 만들기2

--- src/com/javaex/ex05/Ex01.java
+++ src/com/javaex/ex05/Ex01.java
@@ -1,19 +1,15 @@
 package com.javaex.ex05;
 
 public class Ex01 {
-	
+
 	public static void main(String[] args) {
-		
-		int num = (int)(Math.random()*45)+1;
-		
-		
-		System.out.print(num+"\t");
-		System.out.print(num+"\t");
-		System.out.print(num+"\t");
-		System.out.print(num+"\t");
-		System.out.print(num+"\t");
-		System.out.print(num+"\t");
-		
+
+		for (int i = 1; i <= 6; i++) {
+			int num = (int) (Math.random() * 45) + 1;
+			System.out.print(num + "\t");
+
+		}
+
 	}
 
 }
