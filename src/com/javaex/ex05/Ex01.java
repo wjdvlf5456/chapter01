60514d7b14cfe25230aa42c75b7263aa6dc60249
최정필 <wjdvlf1407@gmail.com>
2022-04-20 11:53:49 +0900

    Ex02,Ex03 배열 예제

--- src/com/javaex/ex05/Ex01.java
+++ src/com/javaex/ex05/Ex01.java
@@ -4,11 +4,59 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 
+		/*
 		for (int i = 1; i <= 6; i++) {
 			int num = (int) (Math.random() * 45) + 1;	//	int num = (int) (Math.random() * 45) + 1;
 			System.out.print(num + "\t");				//	int num = (정수빼고 버려줌)(Math.random() * 최댓값) + 최소값;
-
 		}
+		 */
+
+		int no1, no2, no3, no4, no5, no6;
+		
+		no1 = (int)(Math.random()*45)+1;
+		no2 = (int)(Math.random()*45)+1;
+		no3 = (int)(Math.random()*45)+1;
+		no4 = (int)(Math.random()*45)+1;
+		no5 = (int)(Math.random()*45)+1;
+		no6 = (int)(Math.random()*45)+1;
+		
+		System.out.println(no1);
+		System.out.println(no2);
+		System.out.println(no3);
+		System.out.println(no4);
+		System.out.println(no5);
+		System.out.println(no6);
+		
+		System.out.println("=================================");
+		
+		for (int i = 1; i <=6; i++) {
+			int num = (int)(Math.random()*45)+1;
+			System.out.println(num);
+		}
+		
+		/*
+		스태틱, 스택, 힙
+		
+		스태틱
+		
+		
+		Scanner sc = new Scanner(Systemm.in);
+		
+		--------------------------------------
+		스택
+		
+		
+		
+		
+		
+		
+		--------------------------------------
+		힙
+		nextInt();	-->	(기능)
+		
+		
+		 */
+		
 
 	}
 
---
+++ src/com/javaex/ex05/Ex03.java
@@ -0,0 +1,51 @@
+package com.javaex.ex05;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+
+		/*
+		스태틱
+		 
+		------------------------------------------------------------------------------------------
+		스택 
+		 
+		nums
+		-------------
+		|	0*777	|
+		-------------
+		 
+		------------------------------------------------------------------------------------------
+		힙
+		 0*777
+		----
+		|	|
+		-----
+		
+		 */
+
+		int[] nums = new int[6];
+		/*
+		nums[0] = (int) (Math.random() * 45) + 1;
+		
+		nums[1] = (int) (Math.random() * 45) + 1;
+		nums[2] = (int) (Math.random() * 45) + 1;
+		nums[3] = (int) (Math.random() * 45) + 1;
+		nums[4] = (int) (Math.random() * 45) + 1;
+		nums[5] = (int) (Math.random() * 45) + 1;
+		
+		System.out.println(nums[0]);
+		System.out.println(nums[4]);
+		System.out.println("=============================");
+		
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		 */
+
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+
+	}
+
+}
