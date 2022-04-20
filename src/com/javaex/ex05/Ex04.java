adb5cac66be014591c718f4f90e1ac6fbe26eb4d
최정필 <wjdvlf1407@gmail.com>
2022-04-20 14:20:32 +0900

    Ex04 char형 및 boolean형

--- src/com/javaex/ex05/Ex04.java
+++ src/com/javaex/ex05/Ex04.java
@@ -3,9 +3,8 @@ package com.javaex.ex05;
 public class Ex04 {
 
 	public static void main(String[] args) {
-		
-		
-		//	정수형
+
+		// 정수형
 		/*
 		int[] intArray = new int[3];
 		intArray[0] = 3;
@@ -31,8 +30,8 @@ public class Ex04 {
 		}
 		*/
 		System.out.println("==============================");
-		
-		//	실수형
+
+		// 실수형
 		double[] dArray = new double[3];
 
 		dArray[0] = 1.1;
@@ -41,6 +40,33 @@ public class Ex04 {
 
 		for (int i = 0; i < dArray.length; i++) {
 			System.out.println(dArray[i]);
+		}
+		System.out.println("==============================");
+
+		char[] cArray = new char[5];
+
+		cArray[0] = '안';
+		cArray[1] = '녕';
+		cArray[2] = '하';
+		cArray[3] = '세';
+		cArray[4] = '요';
+
+		for (int i = 0; i < cArray.length; i++) {
+			System.out.println(cArray[i]);
+
+		}
+
+		System.out.println("==============================");
+
+		boolean[] bArray = new boolean[3];
+
+		bArray[0] = true;
+		bArray[1] = false;
+		bArray[2] = 3==5;
+
+		for (int i = 0; i < bArray.length; i++) {
+			System.out.println(bArray[i]);
+
 		}
 
 	}
