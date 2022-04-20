b616362f840c0217f309b73ec8b92b01350a1f15
최정필 <wjdvlf1407@gmail.com>
2022-04-20 10:36:25 +0900

    Ex01 로또번호 주석달기

--- src/com/javaex/ex05/Ex01.java
+++ src/com/javaex/ex05/Ex01.java
@@ -5,8 +5,8 @@ public class Ex01 {
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 6; i++) {
-			int num = (int) (Math.random() * 45) + 1;
-			System.out.print(num + "\t");
+			int num = (int) (Math.random() * 45) + 1;	//	int num = (int) (Math.random() * 45) + 1;
+			System.out.print(num + "\t");				//	int num = (정수빼고 버려줌)(Math.random() * 최댓값) + 최소값;
 
 		}
 
